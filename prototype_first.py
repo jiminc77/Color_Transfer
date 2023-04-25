3958adfc1fb8bbfeffd8553d79fcb4f6931eef0d
jimin <jiminc777@gmail.com>
2023-04-25 13:16:27 +0000

    sleep test 4

--- prototype_first.py
+++ prototype_first.py
@@ -177,7 +177,7 @@ else:
 
 if st.button("Start Transfer!"):        
     subprocess.run(['python3', 'transfer.py'])
-    time.sleep(5)
+    time.sleep(10)
     st.image('./outputs/target_cat5_decoder_encoder_skip..jpg')
     # st.write(type(target))
 
