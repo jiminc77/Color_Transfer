6ff4add220a8a3a9fbd7bf2881a714f4a8f931f4
jimin <jiminc777@gmail.com>
2023-05-28 10:45:04 +0000

    test3 - 7.2

--- prototype_second.py
+++ prototype_second.py
@@ -313,7 +313,7 @@ with st.container():
     if target_file:
         target = Image.open(target_file)
         # here!
-        st.write(os.exists('/app/color_transfer/examples/content'))
+        st.write(os.listdir('/app/color_transfer/examples/content'))
         target.save(f'/app/color_transfer/examples/content/{st.session_state.seed}_target.jpeg', 'JPEG')
         # target.save(f"/examples/content/target.jpg", 'JPEG')
         with ic1:
